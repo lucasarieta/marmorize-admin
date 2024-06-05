08c70bab0308f36ae1b44c9038ad3be04896b1da
Lucas <lucasarieta@gmail.com>
2024-06-04 23:55:04 -0300

    fixes

--- src/app/app/layout.tsx
+++ src/app/app/layout.tsx
@@ -1,10 +1,13 @@
 import Sidebar from '@/components/sidebar';
+import { Suspense } from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='relative flex h-screen overflow-hidden'>
       <Sidebar />
-      <main className='w-full p-4 overflow-y-scroll'>{children}</main>
+      <main className='w-full p-4 overflow-y-scroll'>
+        <Suspense>{children}</Suspense>
+      </main>
     </div>
   );
 }
